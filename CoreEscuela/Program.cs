9bb86bb02d450ba9c05ab17920a388b1d8cfac39
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-20 23:25:34 -0600

    ejemplo de creacion de metodo y clase

--- CoreEscuela/Program.cs
+++ CoreEscuela/Program.cs
@@ -2,11 +2,31 @@
 
 namespace CoreEscuela
 {
-    class Program
+
+    class Escuela
     {
-        static void Main(string[] args)
+        public string nombre;
+        public string direccion;
+        public int fechaFundacion;
+
+        public string ceo = "Freddy Vega";
+
+        public void Timbrar()
         {
-            Console.WriteLine("Hello World!");
+            Console.Beep(10000, 3000);
+        }
+    }
+
+    class Program //es una clase programa
+    {
+        static void Main(string[] args) //hay un metodo que se llama main
+        {
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Xino Academy";
+            miEscuela.fechaFundacion = 2022;
+            miEscuela.direccion = "Costa Rica";
+            Console.WriteLine("Timbre!");
+            miEscuela.Timbrar();
         }
     }
 }
