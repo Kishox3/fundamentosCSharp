9dc17d5022d7065c8f58be0d1ee2124384b7e329
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2022-10-23 16:17:07 -0600

    Se le da formato a la primera etapa del proyecto, eliminacion de comentarios de lo aprendido

--- CoreEscuela/Entidades/Escuela.cs
+++ CoreEscuela/Entidades/Escuela.cs
@@ -2,59 +2,36 @@ namespace CoreEscuela.Entidades
 {
     class Escuela
     {
-        string nombre;
+        //Propiedades
+        public string Nombre { get; set; }
 
-        public string Nombre //este "Nombre" es diferente a "nombre" se encargar de registrar los datos y convertirlos en MAYUS
-        {
-            get { return nombre; }
-            set { nombre = value.ToUpper(); }
-        }
-
-        //uso de propiedades
-        //es lo mismo que lo anterior pero de forma mas simple ya que tiene variables internas ocultas
         public int AñoDeCreacion { get; set; }
 
         public string Pais { get; set; }
 
         public string Ciudad { get; set; }
 
-        public TiposEscuela TipoEscuela { get; set; } //vamos a usar enumeraciones para evitar TYPOS 
-        // en lugar de string el tipo de datos sera con el nombre del enum que se creo
+        public TiposEscuela TipoEscuela { get; set; }
 
-        /*
-        public Escuela(string nombre, int año) //constructor de escuelas
-        {
-            this.nombre = nombre; //se utiliza this para poder usar la palabra nombre dos veces
-            // clase       entrada
-            
-            se puede hacer asi sin this:
-            public Escuela(string nombre de Entrada)
-            {
-                nombre = nombreEntrada; /
-            }
-            
-
-            AñoDeCreacion = año;
-            // clase        entrada
-        }
-        */
+        //constructor 1
         public Escuela(string nombre, int año) => (Nombre, AñoDeCreacion) = (nombre, año); //se procede a utilziar una igualacion por tuplas para simplificar el constructor
 
+        //constructor 2
         public Escuela(
-            string nombre, 
-            int año, 
-            TiposEscuela tipo, 
-            string pais = "", 
+            string nombre,
+            int año,
+            TiposEscuela tipo,
+            string pais = "",
             string ciudad = "")
         {
             (Nombre, AñoDeCreacion) = (nombre, año);
             Pais = pais;
-            Ciudad = ciudad; //los parametros opcionales se pueden incluir o no y en el orden que se quiera
+            Ciudad = ciudad;
         }
 
-        //se pueden crear varios constructores con los parametros que se deseen
 
-        public override string ToString() //se sobreescribe el objeto para que no imprima su ruta, sino lo que especificamente quiero que suceda
+        //Override para imprimir toda la info cuando se llame el obj escuela
+        public override string ToString()
         {
             return $"Nombre: {Nombre} \nTipo: {TipoEscuela} \nPais: {Pais} \nCiudad: {Ciudad}"; //signo dolar me permite usar variable dentro de llaves
         }
--- CoreEscuela/Program.cs
+++ CoreEscuela/Program.cs
@@ -7,18 +7,13 @@ namespace CoreEscuela
     {
         static void Main(string[] args)
         {
-            // aca vamos a utilizar la clase Escuela que acabamos de crear en entidades
+            //constructor 2
             var escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
-            pais:"Colombia", ciudad: "Bogota" //los parametros opcionales permiten omitir el orden
-            ); //constructor para usar la clase
+            pais: "Colombia", ciudad: "Bogota" //parametros opcionales
+            );
 
-            escuela.TipoEscuela = TiposEscuela.Primaria; //solo deja escoger la lista de la numeracion
-
-            Console.WriteLine(escuela); //si se imprime el objeto se impre el nombre completo del objeto, no su informacion
-            //por lo tanto en escuela.cs se hizo un override para imprimir todo poniendo escuela [se profundizara mas]
-
-
+            Console.WriteLine(escuela);
         }
     }
-    
+
 }
